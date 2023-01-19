c40732bd38265eb0627bf445ae61eaea660e040e
Ebraheem Alkhars <ebraheem6431@gmail.com>
2023-01-19 18:17:44 +0300

    hello

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,4 +2,20 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    /// Add the alanButton variable
+  private AlanButton alanButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    /// Set up the Alan button
+    AlanConfig config = AlanConfig.builder().setProjectId("9355c5fd01953a0e96b31f49c0f23eaa2e956eca572e1d8b807a3e2338fdd0dc/stage").build();
+    alanButton = findViewById(R.id.alan_button);
+    alanButton.initWithConfig(config);
+  }
+
+}
